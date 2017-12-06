34b63ef8d405b219fd7339425c4eb127c65db889
Alex Ellsworth <alex.ells21@gmail.com>
2017-12-06 12:09:10 -0700

    last thing I needed

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import Browsing from './Components/browsing/Browsing';
+import BrowsingS from './Components/browsing/BrowsingSecret';
+
 import Edit from './Components/Edit/Edit';
 // import Details from './Components/details/Details'
 
@@ -13,6 +15,8 @@ export default (
         <Switch>
             <Route component={Browsing} exact path='/' />
             <Route component={Edit} exact path='/edit/:one' />
+            <Route render={BrowsingS} exact path='/yup' />
+            
             {/* <Route component={Details} exact path='/details' /> */}
         </Switch>
     </div>
